b3087073d055696cd86d7da84dbeaf3bd21b4ab3
Restyled.io <commits@restyled.io>
2020-08-21 00:14:34 +0000

    Restyled by prettier

--- components/Layout.js
+++ components/Layout.js
@@ -1,69 +1,66 @@
-import Link from 'next/link'
-import React, {useEffect, useState} from 'react'
-import {Moon, Sun} from 'react-feather'
-import {Col, Grid, Row} from 'react-flexbox-grid'
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { Moon, Sun } from "react-feather";
+import { Col, Grid, Row } from "react-flexbox-grid";
 
-import {currentDayName} from '../utils/dateUtils'
+import { currentDayName } from "../utils/dateUtils";
 
-const menu =
-    [
-      {
-        path : '/',
-        name : 'dash',
-      },
-      {
-        path : '/blog',
-        name : 'blog',
-      },
-      {
-        path : '/about',
-        name : 'about',
-      },
-      {
-        path : '/uses',
-        name : 'uses',
-      },
-      {
-        path : '/reviews',
-        name : 'reviews',
-      },
-    ]
+const menu = [
+  {
+    path: "/",
+    name: "dash",
+  },
+  {
+    path: "/blog",
+    name: "blog",
+  },
+  {
+    path: "/about",
+    name: "about",
+  },
+  {
+    path: "/uses",
+    name: "uses",
+  },
+  {
+    path: "/reviews",
+    name: "reviews",
+  },
+];
 
-    function Layout({children, isBlog, secondaryPage, noHead = false}) {
-      const onLoadTheme = typeof localStorage !== 'undefined' &&
-                          localStorage.getItem('BLOG_THEME')
-      const [theme, setTheme] = useState(onLoadTheme)
-      const [mounted, setMounted] = useState(false)
-      const switchTheme =
-          () => {
-            const setTo = theme === 'dark' ? 'light' : 'dark'
+function Layout({ children, isBlog, secondaryPage, noHead = false }) {
+  const onLoadTheme =
+    typeof localStorage !== "undefined" && localStorage.getItem("BLOG_THEME");
+  const [theme, setTheme] = useState(onLoadTheme);
+  const [mounted, setMounted] = useState(false);
+  const switchTheme = () => {
+    const setTo = theme === "dark" ? "light" : "dark";
 
-            setTheme(setTo)
-          }
-
-      useEffect(() => {
-        if (onLoadTheme)
-          return
-
-              if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-            setTheme('dark')
-          }
-      }, [])
+    setTheme(setTo);
+  };
 
   useEffect(() => {
-    document.documentElement.setAttribute('data-theme', theme)
+    if (onLoadTheme) return;
 
-  localStorage.setItem('BLOG_THEME', theme)
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
+    }
+  }, []);
 
-    setMounted(true)
-  }, [theme])
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+
+    localStorage.setItem("BLOG_THEME", theme);
+
+    setMounted(true);
+  }, [theme]);
 
   const containerProps = {
-    ...isBlog && { md: 12 },
-    ...!isBlog && { md: 8, mdOffset: 2 },
-  }
+    ...(isBlog && { md: 12 }),
+    ...(!isBlog && { md: 8, mdOffset: 2 }),
+  };
 
-  if (!mounted) return <div />
+  if (!mounted) return <div />;
 
   return (
     <>
@@ -83,38 +80,47 @@ const menu =
                     <a>{name}</a>
                   </Link>
                 </li>
-              ))
-    }</ul>
+              ))}
+            </ul>
           </Col>
 
-    <Col xs = {2} style = {
-      { textAlign: 'right' }
-    }>
-    <button className = "theme-switch-button" onClick = {() => switchTheme()}>{
-      theme === 'dark' ? <Sun />: <Moon />
-    }</button>
-          </Col></Row>
+          <Col xs={2} style={{ textAlign: "right" }}>
+            <button
+              className="theme-switch-button"
+              onClick={() => switchTheme()}
+            >
+              {theme === "dark" ? <Sun /> : <Moon />}
+            </button>
+          </Col>
+        </Row>
       </div>
 
-    <Grid><Row><Col{...containerProps}>{
-        !secondaryPage &&
-        (<h1 className = {`blog-title`} style = {
-           isBlog && { textAlign: 'left' }
-         }>Guilloutran,
-         code<span className = "amp">& </span> things!
-              </h1>)}
+      <Grid>
+        <Row>
+          <Col {...containerProps}>
+            {!secondaryPage && (
+              <h1
+                className={`blog-title`}
+                style={isBlog && { textAlign: "left" }}
+              >
+                Guilloutran, code<span className="amp">& </span> things!
+              </h1>
+            )}
 
-    {children}</Col>
+            {children}
+          </Col>
         </Row>
-    </Grid>
+      </Grid>
 
       <footer>
         <div>No tracking. No ads. Happy {currentDayName()}!</div>
-    <div>& copy;
-{new Date().getFullYear()}</div>
-      </footer><
-    />
-  )
+        <div>
+          & copy;
+          {new Date().getFullYear()}
+        </div>
+      </footer>
+    </>
+  );
 }
 
-export default Layout
+export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,46 +1,55 @@
-import '../styles/base.css'
+import "../styles/base.css";
 
-import {
-  ThemeProvider,
-} from '@chakra-ui/core';
-import Head from 'next/head'
+import { ThemeProvider } from "@chakra-ui/core";
+import Head from "next/head";
 
-import theme from '../styles/theme';
+import theme from "../styles/theme";
 
-function MyApp({Component, pageProps}) {
-  const og = pageProps.data?.og
-  const title = pageProps.data?.title
+function MyApp({ Component, pageProps }) {
+  const og = pageProps.data?.og;
+  const title = pageProps.data?.title;
 
   return (
-      <ThemeProvider theme = {theme}><Head>
-      <meta name = "viewport" content =
-           "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-      <meta property = "og:title" content =
-       {
-         title || `GuillouTran, code & opinion`
-       } />
+    <ThemeProvider theme={theme}>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta
+          property="og:title"
+          content={title || `GuillouTran, code & opinion`}
+        />
         <meta property="og:site_name" content="GuillouTran, code & opinion" />
-      <meta property = "og:description" content =
-       {
-         og ? og.description
-            : `Writing about things I care and my opinion on stuff.`
-       } />
+        <meta
+          property="og:description"
+          content={
+            og
+              ? og.description
+              : `Writing about things I care and my opinion on stuff.`
+          }
+        />
         <meta name="twitter:card" content="summary_large_image" />
-      <meta name = "twitter:site" content = "@telmo" />
-      <meta property = "og:image" content =
-       {
-         og ? og.image : `https://telmo.im/og/default.png`
-       } />
+        <meta name="twitter:site" content="@telmo" />
+        <meta
+          property="og:image"
+          content={og ? og.image : `https://telmo.im/og/default.png`}
+        />
 
-        <script async src="https:/ /
-               platform.twitter.com / widgets.js " charSet=" utf -
-           8 "></script>
+        <script
+          async
+          src="https:/ /
+               platform.twitter.com / widgets.js "
+          charSet=" utf -
+           8 "
+        ></script>
 
-       < title>{title || `GuillouTran: code & opinion`}</title>
+        <title>{title || `GuillouTran: code & opinion`}</title>
       </Head>
 
-      <Component { ...pageProps } />
-    </ThemeProvider>)
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
