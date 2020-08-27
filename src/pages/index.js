3c461c3667e587c4ea946d4adf8822963c73e7ee
Restyled.io <commits@restyled.io>
2020-08-27 00:24:18 +0000

    Restyled by clang-format

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,9 @@ import NowPlaying from "../components/NowPlaying";
 import TopTracks from "../components/TopTracks";
 
 function HomePage() {
-  return (
-    <>
-      <Layout secondaryPage>
-        <br />
-        <br />
-        <NowPlaying />
-        <TopTracks />
-      </Layout>
-    </>
-  );
+  return (<><Layout secondaryPage><br /><br /><NowPlaying /><TopTracks />
+          </Layout>
+    </>);
 }
 
 export default HomePage;
