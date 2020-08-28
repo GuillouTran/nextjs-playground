18a4d003943d5e1c10f506cda86c206748e50294
guilloutran <will@guilloutran.com>
2020-08-28 02:55:41 +0200

    Syntax

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,26 +1,26 @@
-import NextErrorComponent from 'next/error'
-import * as Sentry from '@sentry/browser'
+import NextErrorComponent from "next/error";
+import * as Sentry from "@sentry/browser";
 
 const MyError = ({ statusCode, hasGetInitialPropsRun, err }) => {
   if (!hasGetInitialPropsRun && err) {
     // getInitialProps is not called in case of
     // https://github.com/vercel/next.js/issues/8592. As a workaround, we pass
     // err via _app.js so it can be captured
-    Sentry.captureException(err)
+    Sentry.captureException(err);
   }
 
-  return <NextErrorComponent statusCode={statusCode} />
-}
+  return <NextErrorComponent statusCode={statusCode} />;
+};
 
 MyError.getInitialProps = async ({ res, err, asPath }) => {
   const errorInitialProps = await NextErrorComponent.getInitialProps({
     res,
     err,
-  })
+  });
 
   // Workaround for https://github.com/vercel/next.js/issues/8592, mark when
   // getInitialProps has run
-  errorInitialProps.hasGetInitialPropsRun = true
+  errorInitialProps.hasGetInitialPropsRun = true;
 
   // Running on the server, the response object (`res`) is available.
   //
@@ -37,12 +37,12 @@ MyError.getInitialProps = async ({ res, err, asPath }) => {
 
   if (res?.statusCode === 404) {
     // Opinionated: do not record an exception in Sentry for 404
-    return { statusCode: 404 }
+    return { statusCode: 404 };
   }
   if (err) {
-    Sentry.captureException(err)
-    await Sentry.flush(2000)
-    return errorInitialProps
+    Sentry.captureException(err);
+    await Sentry.flush(2000);
+    return errorInitialProps;
   }
 
   // If this point is reached, getInitialProps was called without any
@@ -50,10 +50,10 @@ MyError.getInitialProps = async ({ res, err, asPath }) => {
   // indicate a bug introduced in Next.js, so record it in Sentry
   Sentry.captureException(
     new Error(`_error.js getInitialProps missing data at path: ${asPath}`)
-  )
-  await Sentry.flush(2000)
+  );
+  await Sentry.flush(2000);
 
-  return errorInitialProps
-}
+  return errorInitialProps;
+};
 
-export default MyError
+export default MyError;
