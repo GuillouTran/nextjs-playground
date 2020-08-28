b35d20417b5843c34467581f577ff18a9a8d19c6
Restyled.io <commits@restyled.io>
2020-08-28 01:35:05 +0000

    Restyled by prettier

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,48 +4,47 @@ import {
   Heading,
   Stack,
   Text,
-  useColorMode
-} from '@chakra-ui/core';
-import NextLink from 'next/link';
-import React from 'react';
+  useColorMode,
+} from "@chakra-ui/core";
+import NextLink from "next/link";
+import React from "react";
 
 const Error = () => {
-  const {colorMode} = useColorMode();
-  const secondaryTextColor = {light : 'gray.700', dark : 'gray.400'};
+  const { colorMode } = useColorMode();
+  const secondaryTextColor = { light: "gray.700", dark: "gray.400" };
 
   return (
-      <Stack
-        as="main"
-  spacing = {8} justifyContent = "center"
-  alignItems = "flex-start"
-  m = "0 auto 4rem auto"
-  maxWidth = "700px" > < Flex
-  flexDirection = "column"
-  justifyContent = "flex-start"
-  alignItems = "flex-start"
-  maxWidth =
-      "700px" >
-      <Heading letterSpacing = "tight" mb = {2} as
-       = "h1" size = "2xl">451 – Unavailable For Legal
-          Reasons<
-              /Heading>
-          <Text color={secondaryTextColor[colorMode]} my={4}>
-            Why show a generic 404 when I can make it sound mysterious? It seems
-            you've found something that used to exist, or you spelled something
-            wrong. I'm guessing you spelled something wrong. Can you double
-            check that URL?
-          </Text>
-      <NextLink href = "/" passHref>< Button as
-      = "a"
-  p = {[1, 4]} w = "250px"
-  fontWeight = "bold"
-              m="3rem auto 0"
-            >
-              Return Home
-            </Button>
-          </NextLink>
-        </Flex>
-      </Stack>
+    <Stack
+      as="main"
+      spacing={8}
+      justifyContent="center"
+      alignItems="flex-start"
+      m="0 auto 4rem auto"
+      maxWidth="700px"
+    >
+      {" "}
+      <Flex
+        flexDirection="column"
+        justifyContent="flex-start"
+        alignItems="flex-start"
+        maxWidth="700px"
+      >
+        <Heading letterSpacing="tight" mb={2} as="h1" size="2xl">
+          451 – Unavailable For Legal Reasons
+        </Heading>
+        <Text color={secondaryTextColor[colorMode]} my={4}>
+          Why show a generic 404 when I can make it sound mysterious? It seems
+          you've found something that used to exist, or you spelled something
+          wrong. I'm guessing you spelled something wrong. Can you double check
+          that URL?
+        </Text>
+        <NextLink href="/" passHref>
+          <Button as="a" p={[1, 4]} w="250px" fontWeight="bold" m="3rem auto 0">
+            Return Home
+          </Button>
+        </NextLink>
+      </Flex>
+    </Stack>
   );
 };
 
