b159e210d1b475dfbc79b1f87b679cbbbfcb0b4b
Restyled.io <commits@restyled.io>
2020-08-30 00:52:41 +0000

    Restyled by clang-format

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -4,22 +4,23 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/no-access-state-in-setstate */
-import React, { PureComponent } from "react";
-import { CopyToClipboard } from "react-copy-to-clipboard";
-import { Check, Copy } from "react-feather";
+import React, {PureComponent} from "react";
+import {CopyToClipboard} from "react-copy-to-clipboard";
+import {Check, Copy} from "react-feather";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { anOldHope } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import {anOldHope} from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const preStyle = {
-  borderRadius: 6,
-  padding: "1.7em",
-  lineHeight: "2.3em",
+  borderRadius : 6,
+  padding : "1.7em",
+  lineHeight : "2.3em",
 };
 
 const codeProps = {
-  style: {
-    fontFamily: `ibm-plex-mono, Consolas, Monaco, 'Lucida Console', 'Liberation Mono', 'DejaVu Sans Mono', 'Bitstream Vera Sans Mono', 'Courier New'`,
-    fontSize: "18.5px",
+  style : {
+    fontFamily :
+        `ibm-plex-mono, Consolas, Monaco, 'Lucida Console', 'Liberation Mono', 'DejaVu Sans Mono', 'Bitstream Vera Sans Mono', 'Courier New'`,
+    fontSize : "18.5px",
   },
 };
 
@@ -28,22 +29,22 @@ class CodeBlock extends PureComponent {
     super(props);
 
     this.state = {
-      removeLines: [],
-      addLines: [],
-      updateLines: [],
-      copiedToClipboard: false,
-      displayCopyButton: false,
+      removeLines : [],
+      addLines : [],
+      updateLines : [],
+      copiedToClipboard : false,
+      displayCopyButton : false,
     };
   }
 
   componentDidMount() {
-    const { language } = this.props;
+    const {language} = this.props;
     const linesObj = language && language.split(":")[1];
 
     if (linesObj) {
       const splittedValues = linesObj.split(",");
       let stateLabel;
-      const linesToUpdate = { removeLines: [], addLines: [], updateLines: [] };
+      const linesToUpdate = {removeLines : [], addLines : [], updateLines : []};
 
       splittedValues.map((lines) => {
         const linesRange = lines.split(",");
@@ -63,11 +64,8 @@ class CodeBlock extends PureComponent {
           }
 
           if (splitted.length > 1) {
-            for (
-              let i = parseInt(splitted[0]);
-              i <= parseInt(splitted[1]);
-              i++
-            ) {
+            for (let i = parseInt(splitted[0]); i <= parseInt(splitted[1]);
+                 i++) {
               linesToUpdate[stateLabel].push(i);
             }
           } else {
@@ -76,7 +74,7 @@ class CodeBlock extends PureComponent {
           }
 
           this.setState({
-            [stateLabel]: [
+            [stateLabel] : [
               ...this.state[stateLabel],
               ...linesToUpdate[stateLabel],
             ],
@@ -87,22 +85,22 @@ class CodeBlock extends PureComponent {
   }
 
   copyToClipboard = () => {
-    this.setState({ ...this.state, copiedToClipboard: true }, () => {
-      setTimeout(() => {
-        this.setState({ ...this.state, copiedToClipboard: false });
-      }, 3500);
+    this.setState({...this.state, copiedToClipboard : true}, () => {
+      setTimeout(
+          () => { this.setState({...this.state, copiedToClipboard : false}); },
+          3500);
     });
   };
 
   toggleCopyButton = () => {
     this.setState({
       ...this.state,
-      displayCopyButton: !this.state.displayCopyButton,
+      displayCopyButton : !this.state.displayCopyButton,
     });
   };
 
   render() {
-    const { language, value } = this.props;
+    const {language, value} = this.props;
     const {
       addLines,
       removeLines,
@@ -113,57 +111,52 @@ class CodeBlock extends PureComponent {
 
     return (
       <div
-        onMouseEnter={() => this.toggleCopyButton()}
-        onMouseLeave={() => this.toggleCopyButton()}
-      >
-        {" "}
-        <SyntaxHighlighter
-          language={language}
-          style={anOldHope}
-          customStyle={preStyle}
-          codeTagProps={codeProps}
-          wrapLines
-          lineProps={(lineNumber) => {
-            const mergedLines = addLines
-              .concat(removeLines)
-              .concat(updateLines);
-            let style = { display: "block" };
+    onMouseEnter = {() => this.toggleCopyButton()} onMouseLeave =
+        {() => this.toggleCopyButton()} > {" "} < SyntaxHighlighter
+    language = {language} style = {anOldHope} customStyle = {
+        preStyle} codeTagProps = {codeProps} wrapLines
+    lineProps =
+        {
+          (lineNumber) => {
+            const mergedLines =
+                addLines.concat(removeLines).concat(updateLines);
+            let style = {display : "block"};
 
             if (mergedLines.includes(lineNumber)) {
-              style = { ...style, margin: "0 -22px", padding: "3px 12px 6px" };
+              style = {...style, margin : "0 -22px", padding : "3px 12px 6px"};
             }
 
             if (removeLines.includes(lineNumber)) {
               style = {
                 ...style,
-                borderLeft: `6px #f9320c solid`,
-                background: `rgba(249, 50, 12, .1)`,
+                borderLeft : `6px #f9320c solid`,
+                background : `rgba(249, 50, 12, .1)`,
               };
             } else if (addLines.includes(lineNumber)) {
               style = {
                 ...style,
-                borderLeft: `6px #3ac569 solid`,
-                background: `rgba(58, 197, 105, .1)`,
+                borderLeft : `6px #3ac569 solid`,
+                background : `rgba(58, 197, 105, .1)`,
               };
             } else if (updateLines.includes(lineNumber)) {
               style = {
                 ...style,
-                borderLeft: `6px #f9c00c solid`,
-                background: `rgba(249, 192, 12, .1)`,
+                borderLeft : `6px #f9c00c solid`,
+                background : `rgba(249, 192, 12, .1)`,
               };
             }
 
-            return { style };
-          }}
-        >
-          {value}
-        </SyntaxHighlighter>
+            return {style};
+          }
+        } >
+        {value}<
+            /SyntaxHighlighter>
         <div className="copy-to-clipboard">
           <CopyToClipboard text={value} onCopy={() => this.copyToClipboard()}>
             <button>{copiedToClipboard ? "Copied ✔" : "Copy"}</button>
-          </CopyToClipboard>
-        </div>
-      </div>
+        </CopyToClipboard>
+        </div><
+        /div>
     );
   }
 }
--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -6,8 +6,8 @@
 import matter from "gray-matter";
 import Link from "next/link";
 import React from "react";
-import { BookOpen } from "react-feather";
-import { Col, Row } from "react-flexbox-grid";
+import {BookOpen} from "react-feather";
+import {Col, Row} from "react-flexbox-grid";
 import ReactMarkdown from "react-markdown";
 import ProgressBar from "react-scroll-progress-bar";
 import readingTime from "reading-time";
@@ -15,11 +15,13 @@ import readingTime from "reading-time";
 import CodeBlock from "../../components/CodeBlock";
 import Layout from "../../components/Layout";
 
-function Posts({ content, data }) {
+function Posts({content, data}) {
   const frontmatter = data;
-  const { title, author } = frontmatter;
-  const avatar = `https://images.weserv.nl/?url=https://unavatar.now.sh/twitter/${author.twitter}&w=40`;
-  const { text } = readingTime(content);
+  const {title, author} = frontmatter;
+  const avatar =
+      `https://images.weserv.nl/?url=https://unavatar.now.sh/twitter/${
+          author.twitter}&w=40`;
+  const {text} = readingTime(content);
 
   return (
     <>
@@ -41,7 +43,8 @@ function Posts({ content, data }) {
 
           <div className="author">
             <a
-              href={`https://twitter.com/${author.twitter}`}
+              href={`https://twitter.com/${
+    author.twitter}`}
               target="_blank"
               rel="noopener noreferrer nofollow"
             >
