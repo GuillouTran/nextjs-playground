a01524ae239c3e8cfea209c5f4943c0cea2667b7
Restyled.io <commits@restyled.io>
2020-08-30 00:54:29 +0000

    Restyled by prettier

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withSourceMaps = require("@zeit/next-source-maps")();
 const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 
 const {
-  NEXT_PUBLIC_SENTRY_DSN : SENTRY_DSN,
+  NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
   SENTRY_ORG,
   SENTRY_PROJECT,
   SENTRY_AUTH_TOKEN,
@@ -16,15 +16,17 @@ const {
   VERCEL_BITBUCKET_COMMIT_SHA,
 } = process.env;
 
-const COMMIT_SHA = VERCEL_GITHUB_COMMIT_SHA || VERCEL_GITLAB_COMMIT_SHA ||
-                   VERCEL_BITBUCKET_COMMIT_SHA;
+const COMMIT_SHA =
+  VERCEL_GITHUB_COMMIT_SHA ||
+  VERCEL_GITLAB_COMMIT_SHA ||
+  VERCEL_BITBUCKET_COMMIT_SHA;
 
 process.env.SENTRY_DSN = SENTRY_DSN;
 const basePath = "";
 
 module.exports = withSourceMaps({
-  serverRuntimeConfig : {rootDir : __dirname},
-  webpack : (config, options) => {
+  serverRuntimeConfig: { rootDir: __dirname },
+  webpack: (config, options) => {
     // In `pages/_app.js`, Sentry is imported from @sentry/browser. While
     // @sentry/node will run in a Node.js environment. @sentry/node will use
     // Node.js-only APIs to catch even more unhandled exceptions.
@@ -49,15 +51,23 @@ module.exports = withSourceMaps({
     // and upload the source maps to sentry.
     // This is an alternative to manually uploading the source maps
     // Note: This is disabled in development mode.
-    if (SENTRY_DSN && SENTRY_ORG && SENTRY_PROJECT && SENTRY_AUTH_TOKEN &&
-        COMMIT_SHA && NODE_ENV === "production") {
-      config.plugins.push(new SentryWebpackPlugin({
-        include : ".next",
-        ignore : [ "node_modules" ],
-        stripPrefix : [ "webpack://_N_E/" ],
-        urlPrefix : `~${basePath}/_next`,
-        release : COMMIT_SHA,
-      }));
+    if (
+      SENTRY_DSN &&
+      SENTRY_ORG &&
+      SENTRY_PROJECT &&
+      SENTRY_AUTH_TOKEN &&
+      COMMIT_SHA &&
+      NODE_ENV === "production"
+    ) {
+      config.plugins.push(
+        new SentryWebpackPlugin({
+          include: ".next",
+          ignore: ["node_modules"],
+          stripPrefix: ["webpack://_N_E/"],
+          urlPrefix: `~${basePath}/_next`,
+          release: COMMIT_SHA,
+        })
+      );
     }
     return config;
   },
@@ -67,10 +77,14 @@ const withSass = require("@zeit/next-sass");
 const withCSS = require("@zeit/next-css");
 const withTM = require("next-transpile-modules");
 
-module.exports = withCSS(withSass(withTM({
-  transpileModules : [ "react-flexbox-grid", "react-syntax-highlighter" ],
-  webpack(config) {
-    config.module.rules.push({test : /\.md$/, use : "raw-loader"});
-    return config;
-  },
-})));
+module.exports = withCSS(
+  withSass(
+    withTM({
+      transpileModules: ["react-flexbox-grid", "react-syntax-highlighter"],
+      webpack(config) {
+        config.module.rules.push({ test: /\.md$/, use: "raw-loader" });
+        return config;
+      },
+    })
+  )
+);
