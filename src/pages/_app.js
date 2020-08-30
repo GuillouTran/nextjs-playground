104dbdda5a87eaf158eedf152a8a161918ce5530
Restyled.io <commits@restyled.io>
2020-08-30 00:59:04 +0000

    Restyled by prettier

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-param-reassign */
 import "../styles/base.css";
 
-import {ThemeProvider} from "@chakra-ui/core";
+import { ThemeProvider } from "@chakra-ui/core";
 import * as Sentry from "@sentry/browser";
-import {RewriteFrames} from "@sentry/integrations";
+import { RewriteFrames } from "@sentry/integrations";
 import getConfig from "next/config";
 import Head from "next/head";
 
@@ -13,20 +13,20 @@ if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
   const config = getConfig();
   const distDir = `${config.serverRuntimeConfig.rootDir}/.next`;
   Sentry.init({
-    enabled : process.env.NODE_ENV === "production",
-    integrations : [
+    enabled: process.env.NODE_ENV === "production",
+    integrations: [
       new RewriteFrames({
-        iteratee : (frame) => {
+        iteratee: (frame) => {
           frame.filename = frame.filename.replace(distDir, "app:///_next");
           return frame;
         },
       }),
     ],
-    dsn : process.env.NEXT_PUBLIC_SENTRY_DSN,
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   });
 }
 
-function MyApp({Component, pageProps}) {
+function MyApp({ Component, pageProps }) {
   const og = pageProps.data?.og;
   const title = pageProps.data?.title;
 
@@ -34,29 +34,28 @@ function MyApp({Component, pageProps}) {
     <ThemeProvider theme={theme}>
       <Head>
         <meta
-  name = "viewport"
-  content =
-      "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /
-      > {" "} < meta
-  property = "og:title"
-  content =
-  {
-    title || `GuillouTran, code & opinion`
-  } />
-        <meta property="og:site_name" content="GuillouTran, code & opinion" / >
-      < meta
-  property = "og:description"
-  content =
-  {
-    og ? og.description : `Writing about things I care and my opinion on stuff.`
-  } />
-        <meta name="twitter:card" content="summary_large_image" / >
-      <meta name = "twitter:site" content = "@telmo" />< meta
-  property = "og:image"
-  content =
-  {
-    og ? og.image : `https://telmo.im/og/default.png`
-  } />
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />{" "}
+        <meta
+          property="og:title"
+          content={title || `GuillouTran, code & opinion`}
+        />
+        <meta property="og:site_name" content="GuillouTran, code & opinion" />
+        <meta
+          property="og:description"
+          content={
+            og
+              ? og.description
+              : `Writing about things I care and my opinion on stuff.`
+          }
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@telmo" />
+        <meta
+          property="og:image"
+          content={og ? og.image : `https://telmo.im/og/default.png`}
+        />
         <script
           async
           src="https:/ /
@@ -66,8 +65,7 @@ function MyApp({Component, pageProps}) {
         <title>{title || `GuillouTran: code & opinion`}</title>
       </Head>
 
-      <Component {
-    ...pageProps} />
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,16 +2,16 @@
 /* eslint-disable no-shadow */
 /* eslint-disable no-unused-vars */
 import dynamic from "next/dynamic";
-import React, {useEffect, useState} from "react";
-import {Col, Row} from "react-flexbox-grid";
-import {config} from "react-spring";
+import React, { useEffect, useState } from "react";
+import { Col, Row } from "react-flexbox-grid";
+import { config } from "react-spring";
 
 import Icon from "../components/Icon";
 import Layout from "../components/Layout";
-import {PRESENT, SKILLS} from "../constants/Stack";
+import { PRESENT, SKILLS } from "../constants/Stack";
 
 const TextTransition = dynamic(() => import("react-text-transition"), {
-  ssr : false,
+  ssr: false,
 });
 
 function About() {
@@ -19,38 +19,45 @@ function About() {
 
   useEffect(() => {
     const intervalId = setInterval(
-        () => setIndex((index) => index + 1),
-        3000 // every 3 seconds
+      () => setIndex((index) => index + 1),
+      3000 // every 3 seconds
     );
   }, []);
 
   return (
     <>
       <Layout secondaryPage>
-        <div style={{
-    marginTop: 50 }}>
+        <div
+          style={{
+            marginTop: 50,
+          }}
+        >
           {" "}
           <h1 className="main-h1 about-h1">
             Telmo{" "}
             <TextTransition
-  text = {SKILLS[index % SKILLS.length]} springConfig = {config.gentle} style =
-  {
-    { display: "inline-block" }
-  } />
-          </h1 > {" "}<div className = "about-intro"><Row>
-      <Col md = {12}>
-          I'm a full-stack engineer with 13+ years of experience .I started my
-              career back in 2007 as a designer,
-  then quickly
-  started coding as
-      well.<br /><br />I
-      've seen it all. In my career I' ve worked with a lot of technologies:
-          PHP,
-      MooTools, jQuery, Ruby on Rails, Angular, ReactJS, GraphQL,
-      and many more.</Col>
-            </Row><hr /><h3>What
-          I 've worked with so far</h3>{" "} <Row style = {{ marginTop: 30 }}>{
-              " "} {PRESENT.map((s) => (
+              text={SKILLS[index % SKILLS.length]}
+              springConfig={config.gentle}
+              style={{ display: "inline-block" }}
+            />
+          </h1>{" "}
+          <div className="about-intro">
+            <Row>
+              <Col md={12}>
+                I'm a full-stack engineer with 13+ years of experience .I
+                started my career back in 2007 as a designer, then quickly
+                started coding as well.
+                <br />
+                <br />I 've seen it all. In my career I' ve worked with a lot of
+                technologies: PHP, MooTools, jQuery, Ruby on Rails, Angular,
+                ReactJS, GraphQL, and many more.
+              </Col>
+            </Row>
+            <hr />
+            <h3>What I 've worked with so far</h3>{" "}
+            <Row style={{ marginTop: 30 }}>
+              {" "}
+              {PRESENT.map((s) => (
                 <Col
                   md={2}
                   xs={4}
